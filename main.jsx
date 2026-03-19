8288eaeae1b383da32bbb550271ba67e85817049
pratyushkandari26 <pratyushkandari1@gmail.com>
2026-03-20 03:17:37 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,7 @@
-// frontend/src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.jsx";
 import "./styles.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
